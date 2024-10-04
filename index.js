63f9dfc03d7dcb4b4504f25547dcae44529faa9c
gungho0619 <tzztntt@gmail.com>
2024-10-04 17:12:44 +1000

    add: send twilio sms

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+// Import the Twilio module
+const twilio = require("twilio");
+
+// Your Twilio Account SID and Auth Token
+const accountSid = process.env.ACCOUNT_SID; // Replace with your Twilio Account SID
+const authToken = process.env.AUTH_TOKEN; // Replace with your Twilio Auth Token
+
+// Your Twilio Verify Service SID
+const verifyServiceSid = process.env.VERITY_SERVICE_SID; // Replace with your Verify Service SID
+
+const phoneNumber = process.env.PHONE_NUMBER;
+
+// Create Twilio client
+const client = twilio(accountSid, authToken);
+
+// Function to send a verification code to a phone number
+const sendVerificationCode = (phoneNumber) => {
+  client.verify.v2
+    .services(verifyServiceSid)
+    .verifications.create({ to: phoneNumber, channel: "sms" }) // 'sms' is used to send via SMS, can also use 'call' for voice
+    .then((verification) =>
+      console.log(
+        `Verification code sent to ${phoneNumber}: ${verification.status}`
+      )
+    )
+    .catch((error) => console.error("Error sending verification code:", error));
+};
+
+// Example: Send a verification code to a specific phone number
+sendVerificationCode(phoneNumber); // Replace with the phone number to which the code should be sent
